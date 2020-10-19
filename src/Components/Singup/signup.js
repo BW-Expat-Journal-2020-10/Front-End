658093fa359b51be1efefaa5019e379af88fcac4
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-10-19 10:57:04 -0700

    initial singup done

---
+++ src/Components/Singup/signup.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import SignupForm from "./SignupForm";
+import axios from "axios";
+
+const initialSignupValues = {
+  username: "",
+  password: "",
+  first_name: "",
+  last_name: "",
+  email: "",
+};
+export default function Signup() {
+  const [signupValues, setSignupValues] = useState(initialSignupValues);
+
+  const inputChange = (e) => {
+    setSignupValues({
+      ...signupValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://expatjournal-api.herokuapp.com/api/auth/register",
+        signupValues
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+
+    setSignupValues(initialSignupValues);
+  };
+
+  return (
+    <SignupForm
+      values={signupValues}
+      change={inputChange}
+      submit={submitForm}
+    />
+  );
+}

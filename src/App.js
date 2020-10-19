0f4cac7d02e8495d2648175cc8fe4589d67be3be
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-19 12:44:06 -0700

    Added token and routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import './App.css';
-import Signup from "./Components/Singup/signup"
-import Signin from "./Components/Signin/Signin"
+import React from "react";
+import "./App.css";
+import Signup from "./Components/Singup/Signup";
+import Signin from "./Components/Signin/Signin";
+import NewPost from "./Components/NewPost/NewPost"
+
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./Components/PrivateRoute"
 
 function App() {
   return (
     <div className="App">
-      <Signup />
-      <Signin />
+      <Router>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+
+        {/* <PrivateRoute exact path="/" component={""} /> */}
+        <Switch>
+          <Route path="/login" component={Signin} />
+          <Route path="/signup" component={Signup}/>
+          <Route path="/newpost" component={NewPost} />
+          <Route exact path="/"/>
+        </Switch>
+
+
+      </Router>
     </div>
   );
 }
 
 export default App;
-
--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -7,6 +7,6 @@ export const axiosWithAuth = () => {
     headers: {
       Authorization: token
     },
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://expatjournal-api.herokuapp.com/apihttp://localhost:5000/api"
   });
 };

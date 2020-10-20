2a3b6cb80b2fc9b459fd380f55122d5f678d6440
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-20 11:26:04 -0700

    PostValues

--- src/Components/MyPosts/MyPosts.js
+++ src/Components/MyPosts/MyPosts.js
@@ -29,7 +29,7 @@ const MyPosts = () => {
             <img src={post.img_url} alt={post.title} />
             <h2>Title: {post.title}</h2>
             <h3>Body: {post.body}</h3>
-            <button postId={post.id} >Edit Post</button>
+            <button postId={post.id} postValues={post} >Edit Post</button>
             <button postId={post.id} >Delete Post</button>
           </div>
         );

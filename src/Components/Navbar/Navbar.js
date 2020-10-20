6df87916e637aca120ed394239c50191cf26be18
bhawnishk1 <mrbhawnish@gmail.com>
2020-10-20 10:07:38 -0700

    Navbar ready

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,24 +1,92 @@
 import React from "react";
+import { Link, NavLink, useHistory } from "react-router-dom";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
+
+const NavContainer = styled.div`
+    height: 70px;
+    display: flex;
+    padding-left: 219px;
+    align-items: center;
+    margin: 10px 10px 0 10px;
+    background-color: lightgray;
+    border-radius: 20px;
+
+    h1 {
+        color: green;
+        font-size: 1.5rem;
+        font-weight: 500;
+        letter-spacing: 0.15rem;
+        line-height: 1;
+        margin: 2rem;
+    }
+    .nav {
+
+        display: flex;
+        margin: 20px;
+        
+        a {
+          color: black;
+            text-decoration: none;
+            margin: 20px;
+
+            &:hover{
+              color: gray;
+          }
+        }
+        
+        a.active{
+          color: gray;
+          background: lightgray;
+      }
+   }
+   .logout {
+     color: red;
+   }
+}
+`;
 
 const Navbar = () => {
+  const welcomeMsg = localStorage.getItem("message");
 
+  const { go } = useHistory();
 
-   return (
-     <NavWrapper>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
-        <Link to="/newpost">New post</Link>
-        <Link to="/homepage">Home</Link>
+  return (
+    <NavContainer>
+      <h1>
+        <i className="fab fa-pagelines"></i> 
+      </h1>
+      <div className="nav">
+        {!localStorage.getItem("token") ? (
+          <NavLink className="link" activeClassName="active" to="/login">
+            Login
+          </NavLink>
+        ) : (
+          <Link
+            className="logout"
+            to="/"
+            onClick={() => {
+              localStorage.clear();
 
-    </NavWrapper>
-   );
+              go(0);
+            }}
+          >
+            Logout
+          </Link>
+        )}
+        {!localStorage.getItem("token") ? (
+          <NavLink className="link" activeClassName="active" to="/signup">
+            SignUp
+          </NavLink>
+        ) : null}
+        <a href=" https://watermyplantsbw4.netlify.app">
+          <span>About Us</span>
+        </a>
+
+        <NavLink to="/myplant">Home</NavLink>
+      </div>
+      <h1>{welcomeMsg}</h1>
+    </NavContainer>
+  );
 };
-
-const NavWrapper = styled.div`
- height: auto;
-
-`;
 export default Navbar;

53db890e194ea6add60e19ed37dbcb93835d4631
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-21 16:10:03 -0700

    Recent Posts properly display

--- src/Components/PostPage/PostPage.js
+++ src/Components/PostPage/PostPage.js
@@ -17,8 +17,10 @@ const PostPage = () => {
     axiosWithAuth()
       .get("/api/posts")
       .then((res) => {
-        console.log(res);
-        setPublicPosts(res.data.reverse());
+        const data = res.data
+        const sorted = data.sort(function(a, b){return a.id - b.id})
+        setPublicPosts(sorted.reverse());
+        // setPublicPosts(res.data);
       })
       .catch((err) => console.log(err));
   };
@@ -34,20 +36,25 @@ const PostPage = () => {
   return (
     <div>
       {publicPosts.map((post, i) => (
-        <div className="main-post" onClick={() => push(`/post/${post.id}`)} key={post.id}>
-          
+        <div
+          className="main-post"
+          onClick={() => push(`/post/${post.id}`)}
+          key={post.id}
+        >
           <div className="post-header">
             <h2>Title: {post.title}</h2>
+
             {users.map((user) => {
               return post.user_id === user.id ? (
-                <h3 key={user.id} className="creator">Created By: {user.username}</h3>
+                <h3 key={user.id} className="creator">
+                  Created By: {user.username}
+                </h3>
               ) : null;
             })}
           </div>
 
           <img className="post" src={post.img_url} alt="oops! no_image" />
           <p>{post.body}</p>
-  
         </div>
       ))}
     </div>
